a642083c3dd6db0afa24b940f2e987add49876a7
khan <ziyodullokhan009@mail.ru>
2026-05-09 16:13:56 +0900

    feat: learning string in python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("==============")
+print("======= number =======")
 # in JAVA, variable is a name storage location!
 # in Python, variable is named references!
 
@@ -7,6 +7,24 @@ count_type = type(count)
 # print("count:", count, count_type)
 print(f"the count: {count} and type: {count_type}")
 
-result1 = count.bit_count() #method 
-result2 = count.numerator # state
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
 print(result1, result2)
+
+print("======= string =======")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1) {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result (4): {result}")
+
+# print(course)
