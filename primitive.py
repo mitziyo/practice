2dfdcd0eb6bdeb9824c9e1372aa3b2a835a22394
khan <ziyodullokhan009@mail.ru>
2026-05-09 16:34:26 +0900

    feat: lear boolean in python

--- primitive.py
+++ primitive.py
@@ -28,3 +28,22 @@ result = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result}")
 
 # print(course)
+
+print("======= boolean =======")
+# function > type() input() bool() int() str()
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()  # son kritildimi
+print(f"the input value is numeric: {result}")
+
+
+# TRUTHY vs FALSY value  
+# TRUTHY > True, 100, -200, "MIT"
+# FALSY > False, 0, "", None
+
+test_falsy = "" or False or None or 0
+print("The test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
