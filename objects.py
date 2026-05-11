1a4e206235f581f5bf9f0862f61a7120ec12b48a
khan <ziyodullokhan009@mail.ru>
2026-05-12 02:22:10 +0900

    feat: learn objects in Python

---
+++ objects.py
@@ -0,0 +1,33 @@
+'''
+OBJECTS
+(1) What is object
+(2) Iterable object & RANGE
+(3) DICTIONARY
+(4) Error handling system
+'''
+
+
+import array # package/module
+import math # package
+from math import ceil # bunda ceilni ozini qolga olamz
+
+print("===== What is object =====")
+# An object has state and method properties.
+# Everything is object in Python!
+
+print(type('Hello World'))
+print(type(100))
+print(type(True))          # bular classdan olingan instance
+print(type(array))
+print(type(math))
+
+# Hamma objectlar maqsadli object hisoblanadi
+
+# Paradigms > Functional Programming & OOP (object oriented programming)
+# OOP 4 CONCEPTS > Abstraction | Encapsulation | Inheritance | Polimorphism
+
+result1 = math.ceil(97.7) # CALL
+print("result1:", result1)  
+
+result2 = ceil(99.7)
+print("result2:", result2)
