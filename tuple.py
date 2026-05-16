9660167f5ea8a4135bff3230b7d7033af8acb8ea
khan <ziyodullokhan009@mail.ru>
2026-05-16 17:00:31 +0900

    feat: unpacing arguments via tuple

--- tuple.py
+++ tuple.py
@@ -24,10 +24,40 @@ print("before fruits", fruits)
 fruits[2] = "melon"
 print("after fruits", fruits)
 
-# we cannot mutate tuple 
+# we cannot mutate tuple
 animals = ("dog", "cat", "fish", "lion")
 tuple_obj = ("MIT", 100, True, None)
 
 print(animals[0])
-animals[0] = "bird"
-print(animals)
+# animals[0] = "bird"
+# print(animals)
+
+
+# try avoid this
+people = "Andrew", "John"
+animals = "dog",
+
+print("======= Unpacing arguments =======")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, *z) = groups
+print(f"the x: {x}, and y: {y} ")
+print("z:", z)
+
+# *args > tuple
+
+
+def calculate(*args):
+    print("*args", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the total value: {total}")
+    return total
+
+
+# CALL
+calculate(1, 7, 2, 3)
+print("--------")
+calculate(0, 2, 300)
+print("--------")
+calculate(5, 7)
