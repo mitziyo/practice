bc5156d6e585089c100c168319e981d313d4f8a2
khan <ziyodullokhan009@mail.ru>
2026-05-16 17:10:28 +0900

    feat: unpacing via dictionary

--- tuple.py
+++ tuple.py
@@ -61,3 +61,17 @@ print("--------")
 calculate(0, 2, 300)
 print("--------")
 calculate(5, 7)
+
+
+print("-------------")
+# **kwargs > dictionary
+
+
+def introduce(**kwargs):
+    print(f"the type(**kwargs) value: {type(kwargs)}")
+    print(f"Hi, I am {kwargs["name"]} and I am {kwargs["age"]} years old!")
+
+
+# CALL
+introduce(name="Justin", age=28)
+introduce(name="Shawn", age=30, single=True)
