6e2f643b47546a07cdbc53212419cc0d051e571a
khan <ziyodullokhan009@mail.ru>
2026-05-19 16:37:58 +0900

    feat: Core Packages study

---
+++ debugging.py
@@ -0,0 +1,146 @@
+'''
+Packages and debuggung
+(1) Python Packages and Core packages
+(2) Package manager & External Package
+(3) Debugging
+'''
+
+import turtle
+print("======= Python Packages and Core packages =======")
+''' Python Packages/Modules: Core, File and External '''
+#  Core Package > https://docs.python.org/3/library
+
+# Core package
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(1)
+t.circle(100)
+
+turtle.done()
+
+my_file = open("material/message.txt", "r")
+try:
+    content = my_file.read()
+    print("content:", content)
+finally:
+    my_file.close()
+
+# with = contex manager
+with open("material/message.txt", "r") as your_file:
+    your_content = your_file.read()
+    print("your_content:", your_content)
+print("DONE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+import turtle
+
+# Screen setup
+screen = turtle.Screen()
+screen.bgcolor("white")
+screen.title("Pizza with Turtle")
+
+# Turtle setup
+t = turtle.Turtle()
+t.speed(1)
+
+# Draw pizza base
+t.penup()
+t.goto(0, -150)
+t.pendown()
+t.color("orange")
+t.begin_fill()
+t.circle(150)
+t.end_fill()
+
+# Draw cheese layer
+t.penup()
+t.goto(0, -130)
+t.pendown()
+t.color("gold")
+t.begin_fill()
+t.circle(130)
+t.end_fill()
+
+# Pepperoni positions
+pepperoni = [
+    (-50, 50), (40, 60), (-70, -20),
+    (60, -10), (0, 0), (-20, -70),
+    (70, -70)
+]
+
+# Draw pepperoni
+t.color("red")
+for x, y in pepperoni:
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.begin_fill()
+    t.circle(20)
+    t.end_fill()
+
+# Slice lines
+t.color("brown")
+t.pensize(3)
+
+for angle in [0, 60, 120]:
+    t.penup()
+    t.goto(0, 0)
+    t.setheading(angle)
+    t.pendown()
+    t.forward(150)
+
+    t.penup()
+    t.goto(0, 0)
+    t.setheading(angle + 180)
+    t.pendown()
+    t.forward(150)
+
+t.hideturtle()
+turtle.done()
+'''
